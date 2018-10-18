8dc51adc2feab8d4022cf59d9334df341093dddd
jeremySavonet <jeremy.savonet@gmail.com>
2018-10-18 14:14:26 +0200

    wip rework

--- include/WestBot/CoreSLAM/Parameters.hpp
+++ include/WestBot/CoreSLAM/Parameters.hpp
@@ -11,10 +11,31 @@ class RobotParameters
 public:
     RobotParameters() = default;
 
-    double r;	    // length wheels' radius
-    double R;	    // half the wheels' axis length
-    int inc;	    // wheels' counters increments per turn
-    double ratio;   // ratio between left and right wheel
+    double wheelRadius() const
+    {
+        return _wheelRadius;
+    }
+
+    double halfWheelAxisLength() const
+    {
+        return _halfWheelAxixLength;
+    }
+
+    int inc() const
+    {
+        return _inc;
+    }
+
+    double ratio() const
+    {
+        return _ratio;
+    }
+
+private:
+    double _wheelRadius;	     // length wheels' radius
+    double _halfWheelAxixLength; // half the wheels' axis length
+    int _inc;	                 // wheels' counters increments per turn
+    double _ratio;               // ratio between left and right wheel
 };
 
 class LaserParameters
@@ -22,12 +43,43 @@ class LaserParameters
 public:
     LaserParameters() = default;
 
-    double offset;  // position of the laser wrt center of rotation
-    int scan_size;  // number of points per scan
-    int angle_min;  // start angle for scan
-    int angle_max;  // end angle for scan
-    int detection_margin; // first scan element to consider
-    double distance_no_detection; // default value when the laser returns 0
+    double offset() const
+    {
+        return _offset;
+    }
+
+    int scanSize() const
+    {
+        return _scanSize;
+    }
+
+    int angleMin() const
+    {
+        return _angleMin;
+    }
+
+    int angleMax() const
+    {
+        return _angleMax;
+    }
+
+    int detectionMargin() const
+    {
+        return _detectionMargin;
+    }
+
+    double distanceNoDetection() const
+    {
+        return _distanceNoDetection;
+    }
+
+private:
+    double _offset;  // position of the laser wrt center of rotation
+    int _scanSize;  // number of points per scan
+    int _angleMin;  // start angle for scan
+    int _angleMax;  // end angle for scan
+    int _detectionMargin; // first scan element to consider
+    double _distanceNoDetection; // default value when the laser returns 0
 };
 
 }
